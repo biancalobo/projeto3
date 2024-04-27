57aaf7323728f70c2b9a011c7a3f71185b040dfc
biancalobo <160778094+biancalobo@users.noreply.github.com>
2024-04-27 01:31:31 +0000

    funcao listar

--- tarefas.c
+++ tarefas.c
@@ -7,12 +7,31 @@ if(*pos >= TOTAL)
       return lista_cheia;
 
 
-ERROS listar(Contato contatos[], int pos);
+ERROS listar(Contato contatos[], int *pos){
+    if(*pos == 0)
+        return CONTATO_INEXISTENTE;
 
+  char informacao[255];
+  printf("Entre com a informação que deseja listar: ");
+  clearBuffer();
+  fgets(informacao, 255, stdin);
 
-ERROS deletar(Contato contatos[], int *pos);
-  if(*pos == 0)
-    return contato_inexistente
+    int encontradas = 0;
+    for(int i=0; i<*pos; i++){
+        printf("Contato: %d\t", i+1);
+        printf("Nome: %d\t", contatos[i].nome);
+        printf("Email: %s\t", contatos[i].email);
+        printf("Telefone: %s\n", contatos[i].telefone);
+        encontradas++;
+    }
 
-  int pos_deletar;
-  printf("Digite o contato que deseja deletar: ");
+    if(encontradas == 0)
+      printf("Nenhuma informação encontrada em '%s'.\n", informacao);
+      return CONTATO_INEXISTENTE;
+    return OK;
+
+  vazio = 0
+
+  if listar == 0
+    printf("%d", listar)
+}
