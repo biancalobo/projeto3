3fc6b4d9cabfef9d20df44a33da853a2402fc3e1
biancalobo <160778094+biancalobo@users.noreply.github.com>
2024-04-27 02:08:30 +0000

    funcao salvar

--- tarefas.c
+++ tarefas.c
@@ -2,17 +2,22 @@
 #include "tarefas.c"
 #include "tarefas.h"
 
-ERROS adicionar(Contato contatos[], int *pos);
-if(*pos >= TOTAL)
-      return lista_cheia;
 
+ERROS salvar(Contato contatos[], int *pos){
+    FILE *f = fopen("tarefas.bin", "wb");
+    if(f == NULL)
+        return ABRIR;
 
-ERROS listar(Contato contatos[], int pos);
+    int qtd = fwrite(contatos, TOTAL, sizeof(Contato), f);
+    if(qtd == 0)
+        return ESCREVER;
 
+    qtd = fwrite(pos, 1, sizeof(int), f);
+    if(qtd == 0)
+        return ESCREVER;
 
-ERROS deletar(Contato contatos[], int *pos);
-  if(*pos == 0)
-    return contato_inexistente
+    if(fclose(f))
+        return FECHAR;
 
-  int pos_deletar;
-  printf("Digite o contato que deseja deletar: ");
+    return OK;
+}
