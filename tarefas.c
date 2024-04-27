3d41a70827100c7cce090eaeb80f9ac5f85794a1
biancalobo <160778094+biancalobo@users.noreply.github.com>
2024-04-27 01:50:19 +0000

    funcao listar

--- tarefas.c
+++ tarefas.c
@@ -2,12 +2,35 @@
 #include "tarefas.c"
 #include "tarefas.h"
 
-ERROS adicionar(Contato contatos[], int *pos);
-if(*pos >= TOTAL)
-  return maximo_contatos;
+ERROS listar(Contato contatos[], int *pos){
+    if(*pos == 0)
+        return CONTATO_INEXISTENTE;
 
-printf("Digite o nome do contato: ");
-scanf("%s", contatos[*pos].nome);
+  char informacao[255];
+  printf("Entre com a categoria que deseja listar: ");
+  clearBuffer();
+  fgets(informacao, 255, stdin);
+
+    int encontradas = 0;
+    for(int i=0; i<*pos; i++){
+        printf("Contato: %d\t", i+1);
+        printf("Nome: %d\t", contatos[i].nome);
+        printf("Email: %s\t", contatos[i].email);
+        printf("Telefone: %s\n", contatos[i].telefone);
+        encontradas++;
+    }
+
+    if(encontradas == 0)
+      printf("Nenhum contato encontrado com a informação '%s'.\n", informacao);
+      return CONTATO_INEXISTENTE;
+}
+    return OK;
+
+  vazio = 0
+
+  if listar == 0
+    printf("%d", listar)
+}
 
 
 
